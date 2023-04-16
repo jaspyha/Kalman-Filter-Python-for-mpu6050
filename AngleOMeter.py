138fe0cda4895b4dfd8d5a6209e386ef7da97250
Jasper Ha <114321807+jaspyha@users.noreply.github.com>
2023-04-15 18:31:37 -0700

    Update AngleOMeter.py

--- AngleOMeter.py
+++ AngleOMeter.py
@@ -21,13 +21,13 @@ GPIO.setup(IMU1, GPIO.OUT)
 GPIO.setup(IMU2, GPIO.OUT)
 GPIO.setup(IMU3, GPIO.OUT)
 
-kalmanX[] = [KalmanAngle(),KalmanAngle(),KalmanAngle()]
-kalmanY[] = [KalmanAngle(),KalmanAngle(),KalmanAngle()]
+kalmanX = [KalmanAngle(),KalmanAngle(),KalmanAngle()]
+kalmanY = [KalmanAngle(),KalmanAngle(),KalmanAngle()]
 
 RestrictPitch = True	#Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
 radToDeg = 57.2957786
-kalAngleX[] = [0,0,0]
-kalAngleY[] = [0,0,0]
+kalAngleX = [0,0,0]
+kalAngleY = [0,0,0]
 #some MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
@@ -78,117 +78,119 @@ def read_raw_data(addr):
 bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
 DeviceAddress = 0x68   # MPU6050 device address
 
-for num in range(2):
-	if num == 0:
-		GPIO.output(IMU1,GPIO.LOW)
-		GPIO.output(IMU2,GPIO.HIGH)
-		GPIO.output(IMU3,GPIO.HIGH)
-	elif num == 1:
-		GPIO.output(IMU1,GPIO.HIGH)
-		GPIO.output(IMU2,GPIO.LOW)
-		GPIO.output(IMU3,GPIO.HIGH)
-	else:
-		GPIO.output(IMU1,GPIO.HIGH)
-		GPIO.output(IMU2,GPIO.HIGH)
-		GPIO.output(IMU3,GPIO.LOW)
+while True:
+	for num in range(2):
+		if num == 0:
+			GPIO.output(IMU1,GPIO.LOW)
+			GPIO.output(IMU2,GPIO.HIGH)
+			GPIO.output(IMU3,GPIO.HIGH)
+		elif num == 1:
+			GPIO.output(IMU1,GPIO.HIGH)
+			GPIO.output(IMU2,GPIO.LOW)
+			GPIO.output(IMU3,GPIO.HIGH)
+		else:
+			GPIO.output(IMU1,GPIO.HIGH)
+			GPIO.output(IMU2,GPIO.HIGH)
+			GPIO.output(IMU3,GPIO.LOW)
 
-	MPU_Init()
+		MPU_Init()
 
-	time.sleep(1)
-	#Read Accelerometer raw value
-	accX = read_raw_data(ACCEL_XOUT_H)
-	accY = read_raw_data(ACCEL_YOUT_H)
-	accZ = read_raw_data(ACCEL_ZOUT_H)
-
-	#print(accX,accY,accZ)
-	#print(math.sqrt((accY**2)+(accZ**2)))
-	if (RestrictPitch):
-		roll = math.atan2(accY,accZ) * radToDeg
-		pitch = math.atan(-accX/math.sqrt((accY**2)+(accZ**2))) * radToDeg
-	else:
-		roll = math.atan(accY/math.sqrt((accX**2)+(accZ**2))) * radToDeg
-		pitch = math.atan2(-accX,accZ) * radToDeg
-	print(roll)
-	kalmanX[num].setAngle(roll)
-	kalmanY[num].setAngle(pitch)
-	gyroXAngle = roll;
-	gyroYAngle = pitch;
-	compAngleX = roll;
-	compAngleY = pitch;
-
-	timer[] = [time.time(),time.time(),time.time()]
-	flag = 0
-
-	if(flag >100): #Problem with the connection
-		print("There is a problem with the connection")
-		flag=0
-		continue
-	try:
 		#Read Accelerometer raw value
 		accX = read_raw_data(ACCEL_XOUT_H)
 		accY = read_raw_data(ACCEL_YOUT_H)
 		accZ = read_raw_data(ACCEL_ZOUT_H)
 
-		#Read Gyroscope raw value
-		gyroX = read_raw_data(GYRO_XOUT_H)
-		gyroY = read_raw_data(GYRO_YOUT_H)
-		gyroZ = read_raw_data(GYRO_ZOUT_H)
-
-		dt = time.time() - timer[num]
-		timer[num] = time.time()
-
+		#print(accX,accY,accZ)
+		#print(math.sqrt((accY**2)+(accZ**2)))
 		if (RestrictPitch):
 			roll = math.atan2(accY,accZ) * radToDeg
 			pitch = math.atan(-accX/math.sqrt((accY**2)+(accZ**2))) * radToDeg
 		else:
 			roll = math.atan(accY/math.sqrt((accX**2)+(accZ**2))) * radToDeg
 			pitch = math.atan2(-accX,accZ) * radToDeg
+		print(roll)
+		kalmanX[num].setAngle(roll)
+		kalmanY[num].setAngle(pitch)
+		gyroXAngle = roll
+		gyroYAngle = pitch
+		compAngleX = roll
+		compAngleY = pitch
 
-		gyroXRate = gyroX/131
-		gyroYRate = gyroY/131
+		timer = [time.time(),time.time(),time.time()]
+		flag = 0
 
-		if (RestrictPitch):
+		if(flag >100): #Problem with the connection
+			print("There is a problem with the connection")
+			flag=0
+			continue
+		try:
+			#Read Accelerometer raw value
+			accX = read_raw_data(ACCEL_XOUT_H)
+			accY = read_raw_data(ACCEL_YOUT_H)
+			accZ = read_raw_data(ACCEL_ZOUT_H)
 
-			if((roll < -90 and kalAngleX[num] >90) or (roll > 90 and kalAngleX[num] < -90)):
-				kalmanX[num].setAngle(roll)
-				complAngleX = roll
-				kalAngleX[num]   = roll
-				gyroXAngle  = roll
+			#Read Gyroscope raw value
+			gyroX = read_raw_data(GYRO_XOUT_H)
+			gyroY = read_raw_data(GYRO_YOUT_H)
+			gyroZ = read_raw_data(GYRO_ZOUT_H)
+
+			dt = time.time() - timer[num]
+			timer[num] = time.time()
+
+			if (RestrictPitch):
+				roll = math.atan2(accY,accZ) * radToDeg
+				pitch = math.atan(-accX/math.sqrt((accY**2)+(accZ**2))) * radToDeg
 			else:
-				kalAngleX[num] = kalmanX[num].getAngle(roll,gyroXRate,dt)
+				roll = math.atan(accY/math.sqrt((accX**2)+(accZ**2))) * radToDeg
+				pitch = math.atan2(-accX,accZ) * radToDeg
 
-			if(abs(kalAngleX)>90):
-				gyroYRate  = -gyroYRate
-				kalAngleY[num]  = kalmanY[num].getAngle(pitch,gyroYRate,dt)
-		else:
+			gyroXRate = gyroX/131
+			gyroYRate = gyroY/131
 
-			if((pitch < -90 and kalAngleY[num] >90) or (pitch > 90 and kalAngleY[num] < -90)):
-				kalmanY[num].setAngle(pitch)
-				complAngleY = pitch
-				kalAngleY[num] = pitch
-				gyroYAngle  = pitch
+			if (RestrictPitch):
+
+				if((roll < -90 and kalAngleX[num] >90) or (roll > 90 and kalAngleX[num] < -90)):
+					kalmanX[num].setAngle(roll)
+					complAngleX = roll
+					kalAngleX[num]   = roll
+					gyroXAngle  = roll
+				else:
+					kalAngleX[num] = kalmanX[num].getAngle(roll,gyroXRate,dt)
+
+				if(abs(kalAngleX[num])>90):
+					gyroYRate  = -gyroYRate
+					kalAngleY[num]  = kalmanY[num].getAngle(pitch,gyroYRate,dt)
 			else:
-				kalAngleY[num] = kalmanY[num].getAngle(pitch,gyroYRate,dt)
 
-			if(abs(kalAngleY[num])>90):
-				gyroXRate  = -gyroXRate
-				kalAngleX[num] = kalmanX[num].getAngle(roll,gyroXRate,dt)
+				if((pitch < -90 and kalAngleY[num] >90) or (pitch > 90 and kalAngleY[num] < -90)):
+					kalmanY[num].setAngle(pitch)
+					complAngleY = pitch
+					kalAngleY[num] = pitch
+					gyroYAngle  = pitch
+				else:
+					kalAngleY[num] = kalmanY[num].getAngle(pitch,gyroYRate,dt)
 
-		#angle = (rate of change of angle) * change in time
-		gyroXAngle = gyroXRate * dt
-		gyroYAngle = gyroYAngle * dt
+				if(abs(kalAngleY[num])>90):
+					gyroXRate  = -gyroXRate
+					kalAngleX[num] = kalmanX[num].getAngle(roll,gyroXRate,dt)
 
-		#compAngle = constant * (old_compAngle + angle_obtained_from_gyro) + constant * angle_obtained from accelerometer
-		compAngleX = 0.93 * (compAngleX + gyroXRate * dt) + 0.07 * roll
-		compAngleY = 0.93 * (compAngleY + gyroYRate * dt) + 0.07 * pitch
+			#angle = (rate of change of angle) * change in time
+			gyroXAngle = gyroXRate * dt
+			gyroYAngle = gyroYAngle * dt
 
-		if ((gyroXAngle < -180) or (gyroXAngle > 180)):
-			gyroXAngle = kalAngleX
-		if ((gyroYAngle < -180) or (gyroYAngle > 180)):
-			gyroYAngle = kalAngleY
+			#compAngle = constant * (old_compAngle + angle_obtained_from_gyro) + constant * angle_obtained from accelerometer
+			compAngleX = 0.93 * (compAngleX + gyroXRate * dt) + 0.07 * roll
+			compAngleY = 0.93 * (compAngleY + gyroYRate * dt) + 0.07 * pitch
 
-		#print("Angle X: " + str(kalAngleX[num])+"   " +"Angle Y: " + str(kalAngleY[num]))
-		#print(str(roll)+"  "+str(gyroXAngle)+"  "+str(compAngleX)+"  "+str(kalAngleX)+"  "+str(pitch)+"  "+str(gyroYAngle)+"  "+str(compAngleY)+"  "+str(kalAngleY))
-		time.sleep(0.005)
+			if ((gyroXAngle < -180) or (gyroXAngle > 180)):
+				gyroXAngle = kalAngleX
+			if ((gyroYAngle < -180) or (gyroYAngle > 180)):
+				gyroYAngle = kalAngleY
 
-print(str(kalAngleX[0]) + "  " + str(kalAngleX[1]) + " " + str(kalAngleX[2]))
+			#print("Angle X: " + str(kalAngleX[num])+"   " +"Angle Y: " + str(kalAngleY[num]))
+			#print(str(roll)+"  "+str(gyroXAngle)+"  "+str(compAngleX)+"  "+str(kalAngleX)+"  "+str(pitch)+"  "+str(gyroYAngle)+"  "+str(compAngleY)+"  "+str(kalAngleY))
+			time.sleep(0.005)
+		except Exception as exc:
+			print(exc)
+
+	print("IMU 1: " + str(kalAngleX[0]) + ",  IMU2: " + str(kalAngleX[1]) + ", IMU3: " + str(kalAngleX[2]))
